6efeff7286a58424b03ff25eaa0191784beeb63a
git on brains <git@brains>
2015-08-28 11:07:39 +0200

    First commit. Some functions for manipulating and working with filenames.

---
+++ Filename.h
@@ -0,0 +1,95 @@
+
+#ifndef _FILENAME_
+#define _FILENAME_
+
+#include <string>
+
+std::string getFilenameOnly(const std::string &strFullFilePath)
+{
+	//try find forward slash
+	size_t lastSlashPos = strFullFilePath.find_last_of("/");
+
+	//if it doesn't exist try to find backlash
+	if(lastSlashPos == std::string::npos)
+	{
+		lastSlashPos = strFullFilePath.find_last_of("\\");
+	}
+
+	//if there is still nothing found then filename is just input string
+	if(lastSlashPos == std::string::npos)
+	{
+		return strFullFilePath;
+	}
+
+	//Otherwise take the substring and return
+	return strFullFilePath.substr(lastSlashPos + 1);
+}
+
+std::string getFileDirectory(const std::string &strFullFilePath)
+{
+	//try find forward slash
+	size_t lastSlashPos = strFullFilePath.find_last_of("/");
+
+	//if it doesn't exist try to find backlash
+	if(lastSlashPos == std::string::npos)
+	{
+		lastSlashPos = strFullFilePath.find_last_of("\\");
+	}
+
+	//if there is still nothing found then filename is just input string
+	if(lastSlashPos == std::string::npos)
+	{
+		return strFullFilePath;
+	}
+
+	//Otherwise take the substring and return
+	return strFullFilePath.substr(0, lastSlashPos);	
+}
+
+std::string createFullFilePath(const std::string &strFileDirectory, const std::string &strFilenameOnly)
+{
+	//This function only checks whether the directory has a trailing slash and adds it as necessary
+
+	std::string strFullFilePath = strFileDirectory;
+	bool bForwardSlash = false;
+
+	//try find forward slash
+	size_t lastSlashPos = strFullFilePath.find_last_of("/");
+
+	//if it doesn't exist try to find backlash
+	if(lastSlashPos == std::string::npos)
+	{
+		lastSlashPos = strFullFilePath.find_last_of("\\");
+		if(lastSlashPos == std::string::npos)
+		{
+			//If not slashes are found determine by OS
+#ifdef __unix__
+			bForwardSlash = true;
+#endif
+
+			//Otherwise it is windows so leave as false as set above
+		}
+		//Found forward slash leave as false as set above
+	}
+	else
+	{
+		//Otherwise we found a forward slash so mark to use it
+		bForwardSlash = true;
+	}
+
+	if(bForwardSlash)
+	{
+		if(strFullFilePath[strFullFilePath.length()-1] != '/')
+			strFullFilePath.append("/");
+	}
+	else
+	{
+		if(strFullFilePath[strFullFilePath.length()-1] != '\\')
+			strFullFilePath.append("\\");
+	}
+	strFullFilePath.append(strFilenameOnly);
+
+	return strFullFilePath;
+}
+
+#endif //_FILENAME_
